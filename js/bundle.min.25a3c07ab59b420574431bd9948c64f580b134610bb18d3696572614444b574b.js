329fa4a423ef7f782d5d3b18ab7e3e63a50668bd
gempir <gempir@users.noreply.github.com>
2021-03-08 17:36:29 +0000

    deploy: 8699a0acee13ed748e0f59ce0204edfa25abd0a0

---
+++ js/bundle.min.25a3c07ab59b420574431bd9948c64f580b134610bb18d3696572614444b574b.js
@@ -0,0 +1,1 @@
+const theme=document.querySelector('.theme-input'),initialState=localStorage.getItem('themeState')=='true';theme.checked=initialState,setTheme(),theme.addEventListener('change',function(){setTheme(),localStorage.setItem('themeState',theme.checked)});function setTheme(){document.documentElement.setAttribute("data-theme",theme.checked?"dark":"light")}
--- js/bundle.min.fa5df6dfb0026e044bfb59cd9288a110821332b66c209486ac862e18b349a135.js
+++
@@ -1,1 +0,0 @@
-;const theme=document.querySelector('.theme-input');const initialState=localStorage.getItem('themeState')=='true';theme.checked=initialState;setTheme();theme.addEventListener('change',function(){setTheme();localStorage.setItem('themeState',theme.checked);});function setTheme(){document.documentElement.setAttribute("data-theme",theme.checked?"dark":"light");}
